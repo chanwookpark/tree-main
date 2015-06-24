0b75293e801b24b6c5b752f74a3b27ce7aa3e228
chanwook <chanwook.god@gmail.com>
2015-06-24 19:12:21 +0900

    수정 및 저장 액션 붙임 (DB 붙이기 전. UI 수정 전)

--- src/main/java/wiki/tree/main/editor/DocumentEditorController.java
+++ src/main/java/wiki/tree/main/editor/DocumentEditorController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import r2.dustjs.spring.DustModel;
 
 /**
@@ -12,9 +13,25 @@ import r2.dustjs.spring.DustModel;
 @Controller
 public class DocumentEditorController {
 
+    String content;
+
     @RequestMapping(value = "/doc/{docId}/edit", method = RequestMethod.GET)
-    public String viewEditor(@PathVariable("docId") String docId, DustModel model) {
-        model.put("content", "블라블라 컨텐츠");
+    public String viewEditor(@PathVariable String docId, DustModel model) {
+        model.put("docId", docId);
+        model.put("content", content);
         return "editor";
+    }
+
+    @RequestMapping(value = "/doc/{docId}/save", method = RequestMethod.POST)
+    public String save(@PathVariable String docId, @RequestParam("content") String content) {
+        this.content = content;
+        return "redirect:/doc/{docId}";
+    }
+
+    @RequestMapping(value = "/doc/{docId}", method = RequestMethod.GET)
+    public String view(@PathVariable String docId, DustModel model) {
+        model.put("docId", docId);
+        model.put("content", content);
+        return "view";
     }
 }
